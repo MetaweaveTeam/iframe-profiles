a702559f5558ef9efedf230c277c4e24345eddea
André Nunes <hello@anuness.dev>
2023-01-13 13:57:53 +0000

    Add webpack server and html build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const html = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+});
 module.exports = {
-  entry: "./src/main.js",
+  mode: "development",
+  entry: ["./src/main.js"],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [html],
 };
