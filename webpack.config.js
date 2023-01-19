49bde64d6ea2b4d4b4aed2bac983899a3d3b50c8
André Nunes <hello@anuness.dev>
2023-01-19 12:27:03 +0000

    Add minify html to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const html = new HtmlWebpackPlugin({
   template: "./src/index.html",
+  // minify
+  minify: {
+    collapseWhitespace: true,
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    useShortDoctype: true,
+    minifyCSS: true,
+    minifyJS: true,
+  },
 });
 module.exports = {
   mode: "development",
